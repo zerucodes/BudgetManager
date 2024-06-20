42ea2418d0fc1222cf63e5a8f5eaf87fdd643293
Zeru Tadesse <tadesszx@gmail.com>
2024-06-20 00:27:25 -0700

    DB aggregation

--- aggregator.py
+++ aggregator.py
@@ -4,6 +4,8 @@ import os
 import pandas as pd
 from collections import defaultdict
 from dateutil import parser 
+import sqlite3
+from datetime import date
 # Define common regex patterns
 re_header = re.compile(r'(.*date)(.*description)(.*amount)',re.IGNORECASE)
 re_date = re.compile(r'\d{1,2}/\d{1,2}/\d{1,4}')
@@ -74,7 +76,7 @@ def normalize_charges(file_path, headers,indeces,invert):
                 
                 if invert:
                     amount *= -1
-                normalized_row = [date,amount,desc]
+                normalized_row = [date,desc,amount]
               
                 normalized_data.append(normalized_row)
             except Exception as e:
@@ -83,18 +85,30 @@ def normalize_charges(file_path, headers,indeces,invert):
     
     return normalized_data
 
-# def process_files(file_paths):
-#     for file_path in file_paths:
-#         print(f"Processing file: {file_path}")
-#         headers,indeces,invert = identify_vendor(file_path)
-#         normalized_data = normalize_charges(file_path, headers,indeces,invert)
-#         for row in normalized_data:
-#             print(row)
+
 def save_to_csv(transactions, csv_path):
     df = pd.DataFrame(transactions, columns=["Date", "Description", "Amount"])
     csv_path = csv_path.replace(".pdf",".csv")
     df.to_csv(csv_path, index=False)
 
+def save_to_db(transactions):
+    conn = sqlite3.connect('local.sqlite')
+    cursor = conn.cursor()
+
+    # SQL command to insert a new record
+    insert_sql = """
+    INSERT INTO transactions (date, description, amount)
+    VALUES (?, ?, ?)
+    """
+
+    for t in transactions:
+        data = (t[0], t[1], t[2]) 
+        try:
+            cursor.execute(insert_sql, data)
+        except sqlite3.Error as e:
+            print(f"An error occurred: {e}")
+    conn.commit()
+    conn.close()
 
 def process_dir(input_dir='bin',file_extension='.csv'):
     normalized_data = []
@@ -106,6 +120,7 @@ def process_dir(input_dir='bin',file_extension='.csv'):
                 headers,indeces,invert = identify_vendor(input_path)
                 normalized_data.extend(normalize_charges(input_path, headers,indeces,invert))
     save_to_csv(normalized_data,'out/aggregated.csv')
+    save_to_db(normalized_data)
 
 
 process_dir()
